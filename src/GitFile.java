77255be11346847b4d788b2d1e93adb489b2958f
hanjietao <hjtxyr@163.com>
2019-05-09 11:33:53 +0800

    Update GitFile.java
    modify line 3 from remote web

--- src/GitFile.java
+++ src/GitFile.java
@@ -6,6 +6,6 @@ public class GitFile {
 
     //2... [local 1]>>  modify it content，the way you write is wrong ,I modify it using correct way[remote web]
 
-    //3...
+    //3... modify line 3 [remote web]
 
 }
