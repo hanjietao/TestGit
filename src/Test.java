736dff87389885691b88f89804b999ecd54fcc37
hanjietao <hjtxyr@163.com>
2019-05-09 09:34:49 +0800

    Test

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+
+
+public class Test {
+
+    private static final String KEY_PERSON = "hanjietao";
+
+
+    public boolean ifNeedToWork(String name){
+        if(KEY_PERSON.equals(name)){
+            return false;
+        }
+        return true;
+    }
+
+}
