bdefd2a62ad09d2c57edbf43122b22da613b7e4d
hanjietao <hjtxyr@163.com>
2019-05-09 09:54:31 +0800

    Test conflict test

--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,18 @@
-
+import java.util.HashMap;
+import java.util.Hashtable;
 
 public class Test {
 
-    private static final String KEY_PERSON = "hanjietao";
+    private static final String KEY_PERSON = "hunting human";
 
 
     public boolean ifNeedToWork(String name){
         if(KEY_PERSON.equals(name)){
             return false;
         }
+
+        Hashtable h = new Hashtable();
+        HashMap<String,Object> hm = new HashMap<>();
 
         return true;
     }
