8c4a5d1fde635b8ad5faa5d0f2cb78df00f53965
hanjietao <hjtxyr@163.com>
2019-05-09 09:53:32 +0800

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,9 @@
 public class Test {
 
     private static final String KEY_PERSON = "hanjietao";
+    public static void main(String[] args){
+        System.out.println("I don't have to do that.");
+    }
 
 
     public boolean ifNeedToWork(String name){
